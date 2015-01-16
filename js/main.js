bb105fb114ea3cea3d56afb80a1ccf995053ad13
Charles Lueker <charleslueker@yahoo.com>
2015-01-15 23:44:16 -0600

    Thursday's game

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+
+var guess = prompt("The computer has generated a number between one and one-hundred.  Try to guess the number.");
+
+var game = function() {
+	// return 
+   var x = Math.ceil(Math.random()*(.1,.9)*100); 
+}
+
+
+// var =false;
+
+ //   while (guess===false) {
+
+ //   if (guess > game) 
+ //   {
+ //   	guess = prompt("Too high. Try again.");
+ //   }
+
+ //   else if (guess < game) 
+ //   {
+ //   	guess = prompt("Too low. Try again.");
+ //   }
+
+	// else 
+ //   {
+ //   	confirm ("YOU WIN!"); 
+ //   	guess===true;
+ //    }
+}
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
+

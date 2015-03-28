aa2ff4bdfa8e278837b63d8fc317c029bf24294e
Charles Lueker <charleslueker@yahoo.com>
2015-03-28 15:51:52 -0500

    Added bootstrap and a column for tracking the number of turns taken

--- js/main.js
+++ js/main.js
@@ -1,33 +1,65 @@
+$(document).ready(function() {
 
+var numBer = Math.ceil(Math.random() * 100);
+console.log(numBer);
 
-var guess = prompt("The computer has generated a number between one and one-hundred.  Try to guess the number.");
-
-var game = function() {
-	// return 
-   var x = Math.ceil(Math.random()*(.1,.9)*100); 
+function generateNum() {
+	var randomNum = Math.ceil(Math.random() * 100);
+	return randomNum;
 }
 
+function numberGame(value, randomNum) {
+	
+	value = $('input').val();
+	var win = false;
+	var score = 0;
+	var turns = 0;
 
-// var =false;
+		if (isNaN(value)) {
+			throw "You did not enter a number";
+		}	
 
- //   while (guess===false) {
+		if (value == numBer) {
+			$('.win-lose-box').html('YOU WIN!');
+			score++;
+			$('#score-box').html(parseInt($('#score-box').html(), 10)+1);
+			turns++;
+			$('#turn-box').html(parseInt($('#turn-box').html(), 10)+1)
+		}
 
- //   if (guess > game) 
- //   {
- //   	guess = prompt("Too high. Try again.");
- //   }
+		else if (value > numBer) {
+			$('.win-lose-box').html('Too high, guess again'); 
+			$('input').val('');
+			turns++;
+			$('#turn-box').html(parseInt($('#turn-box').html(), 10)+1)
+		}
 
- //   else if (guess < game) 
- //   {
- //   	guess = prompt("Too low. Try again.");
- //   }
+		else {
+			$('.win-lose-box').html('Too low, guess again');
+			$('input').val('');
+			turns++;
+			$('#turn-box').html(parseInt($('#turn-box').html(), 10)+1)
+		}
 
-	// else 
- //   {
- //   	confirm ("YOU WIN!"); 
- //   	guess===true;
- //    }
+	return score;
+	return turns;
 }
+
+	$('#start-button').click(function(event) {
+		var value = $('input').val();
+		numberGame(value, numBer);
+	});
+
+	$('#play-again').click(function(event) {
+
+		$('input').val('');
+		numBer = generateNum();
+		console.log(numBer);
+	});	
+	
+});
+
+
 
 
 
